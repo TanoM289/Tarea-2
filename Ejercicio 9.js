cd6a92cc5facdacc8c2864c40c8cff08d3625217
TanoM289 <100097291+TanoM289@users.noreply.github.com>
2025-02-10 15:06:37 -0700

    Create Ejercicio 9.js

---
+++ Ejercicio 9.js
@@ -0,0 +1,5 @@
+// Simular el lanzamiento de un dado de 6 caras
+let lanzamiento = Math.floor(Math.random() * 6) + 1;
+
+// Mostrar el resultado en la consola
+console.log(`El resultado del lanzamiento del dado es: ${lanzamiento}`);
